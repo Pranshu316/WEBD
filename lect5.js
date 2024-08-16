6ee7e8f6b66561ac373d8a082088eaeb9496d3c8
Pranshu316 <pranshukashyap31@gmail.com>
2024-08-16 10:21:20 +0530

    Create lect5.js

---
+++ lect5.js
@@ -0,0 +1,66 @@
+// var name = "student name"
+// console.log(name)
+// let department ="CS"
+// department="CS section I"
+// console.log("~department:",department)
+// const year=2024
+// // const year=2025
+// console.log(year)
+// const value=4*4
+// const dob ="4-5-2008"
+// const college="GLA University"
+// const updatedkey = 'dob'
+// const student = [{
+//     name: "samp",
+//     college,
+//     dob,
+// },
+// {
+//     name: "harsh",
+//     college,
+//     dob,
+// },
+// {
+//     name: "vivek",
+//     college,
+//     dob,
+// },
+// {
+//     name: "samp",
+//     college,
+//     dob,
+// },
+// ];
+// const studentcopy = {...student}
+// student.name="sample1"
+// // studentcopy.dob = "updatedData"
+// student[updatedkey] = "updateddata"
+// console.log("~student:",student)
+
+// let studentName="studentName"
+
+// const student = {
+//     name: studentName,
+//     dob:
+
+// };
+// console.log("~student:",student)
+// let registarNumber = "ENG"
+// if(true){
+//     registarNumber = "MBBS"
+// }
+// const number = {
+//     name=
+// }
+const dob ="4-5-2008"
+const college="GLA University"
+const currentyear =2024
+
+const student={
+    studentName:"sa",
+    collegeName,
+    dob,
+    
+    
+
+};
